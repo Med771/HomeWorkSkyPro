7a6e39d5337237194d943d1b18fa0d2596db7d1b
Med771 <mm7737333@gmail.com>
2024-10-29 19:09:10 +0300

    HomeWork 10

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        // I used one array for all tasks
-        int[] expenditure = {502323, 83923, 98298, 21927, 28398};
+        // I will create: firstName, middleName, lastName, and after I create fullName.
+        // It will make it easier to do the 1st and 2nd task
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = firstName + " " + middleName + " " + lastName;
 
         // task 1
-        int totalExpenditure = 0;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
-        for (int waste : expenditure) {
-            totalExpenditure += waste;
-        }
+        //task 2
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
-        System.out.println("Сумма трат за месяц составила " + totalExpenditure + " рублей");
+        //task 3
+        fullName = "Иванов Семён Семёнович";
 
-        // task 2
-        int minExpenditure = expenditure[0];
-        int maxExpenditure = expenditure[0];
-
-        for (int i = 1; i < expenditure.length; i++) {
-            if (expenditure[i] < minExpenditure) {
-                minExpenditure = expenditure[i];
-            }
-            if (expenditure[i] > maxExpenditure) {
-                maxExpenditure = expenditure[i];
-            }
-        }
-
-        System.out.println("Минимальная сумма трат за неделю составила " + minExpenditure + " рублей.");
-        System.out.println("Максимальная сумма трат за неделю составила " + maxExpenditure + " рублей");
-
-        // task 3
-        // We already have totalExpenditure, so we will perform the calculations at once
-        float averageSpend = (float) totalExpenditure / expenditure.length;
-
-        System.out.println("Средняя сумма трат за месяц составила " + averageSpend + " рублей");
-
-        // task 4
-        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-        }
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replaceAll("ё", "е"));
     }
 }
