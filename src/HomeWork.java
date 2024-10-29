ae50dc4e900b999061ac8c9552a236b03678acdc
Med771 <mm7737333@gmail.com>
2024-10-29 23:19:27 +0300

    HomeWork 11 Create Homework.java

---
+++ src/HomeWork.java
@@ -0,0 +1,59 @@
+import java.time.LocalDate;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        // task 1
+        isLeapYear(2024);
+
+        // task 2
+        getUrlDownloadApp(1, 2023);
+
+        // task 3
+        int days = getDayByDistance(95);
+
+        System.out.println(days);
+    }
+
+    public static void isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year +" год является високосным");
+            return;
+        }
+
+        System.out.println(year + " год не является високосным");
+    }
+
+    public static void getUrlDownloadApp(int clientDeviceOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+
+        if (clientDeviceOS == 0 && currentYear == clientDeviceYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            return;
+        }
+        if (clientDeviceOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            return;
+        }
+
+        if (clientDeviceOS == 1 && currentYear == clientDeviceYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            return;
+        }
+
+        System.out.println("Установите облегченную версию приложения для Android по ссылке");
+    }
+
+    public static int getDayByDistance(int deliveryDistance ) {
+        if (deliveryDistance < 20) {
+            return 1;
+        }
+        if (deliveryDistance < 60) {
+            return 2;
+        }
+        if (deliveryDistance < 100) {
+            return 3;
+        }
+
+        return 0;
+    }
+}
